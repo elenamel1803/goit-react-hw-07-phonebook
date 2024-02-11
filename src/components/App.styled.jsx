797cb64e54b5be5347843c31f38dc550e495362b
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-02-12 00:10:56 +0200

    update

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -2,8 +2,10 @@ import styled from '@emotion/styled';
 
 export const Title = styled.h1`
   text-align: center;
+  margin-bottom: 10px;
 `;
 
 export const ContactsTitle = styled.h2`
   text-align: center;
+  margin-bottom: 20px;
 `;
